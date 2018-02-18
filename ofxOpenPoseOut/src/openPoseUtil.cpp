d4d0fdb5983b55e4a08a7bc94e33d8d92cff7587
David Gordon <heximhotep@gmail.com>
2018-02-18 13:55:10 -0500

    updated readme

--- ofxOpenPoseOut/src/openPoseUtil.cpp
+++ ofxOpenPoseOut/src/openPoseUtil.cpp
@@ -78,12 +78,7 @@ PoseScenes imageExtraction(struct OpenPoseArguments args)
 		int numPoses = poseKeypoints.getSize(0);
 		int numJoints = poseKeypoints.getSize(1);
 		PoseScene scene;
-		if (numPoses > 10)
-		{
-			char saveName[100];
-			sprintf(saveName, "waco_%04d.jpg", i);
-			testImg.saveImage(saveName, ofImageQualityType::OF_IMAGE_QUALITY_BEST);
-		}
+
 		scene.sceneName = "__SCENE__" + ofToString(i);
 		for (int i = 0; i < numPoses; i++)
 		{
